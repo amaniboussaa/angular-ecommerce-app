415340aa09c4ccf5a8c9c272f41262a3e8dbe7b2
amaniboussaa <amani.boussaa16@gmail.com>
2025-04-26 23:37:34 +0100

    implement angular routing'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProductListComponent],
-  template: `<app-header />
-  <app-product-list />
+  imports: [HeaderComponent, RouterOutlet],
+  template: `
+    <app-header />
+    <router-outlet />
   `,
   styles: [],
 })
